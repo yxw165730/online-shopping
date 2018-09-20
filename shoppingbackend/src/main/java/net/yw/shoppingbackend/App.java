b700c5607dcf4e8f9461461d9ac4f08315428ede
yxw165730 <yxw165730@gmail.com>
2018-09-20 15:09:38 -0500

    After Configration

---
+++ shoppingbackend/src/main/java/net/yw/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.yw.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
